5e40d8e3b650e2c880feeb5df5ea5e49550f7acf
kenway214 <kenway214@outlook.com>
2025-11-01 12:42:11 +0530

    GameBar: Add missing ViewGroup import for font optimization
    Add missing import that was needed for the applyTypefaceToOverlay()
    method introduced in the previous commit.

--- src/com/android/gamebar/GameBar.kt
+++ src/com/android/gamebar/GameBar.kt
@@ -22,6 +22,7 @@ import android.view.GestureDetector
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.TextView
